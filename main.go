5c2f536da26cccba8c683a560dc3261887d3a6ad
Wagner Riffel <wgrriffel@gmail.com>
2019-02-04 23:15:26 -0200

    add -w option

--- main.go
+++ main.go
@@ -56,6 +56,8 @@ func simg(w io.Writer, img simage) error {
 	return nil
 }
 
+var write = flag.Bool("w", false, "write to source instead of stdout")
+
 func main() {
 	flag.Usage = func() {
 		fmt.Fprintf(os.Stderr, "usage: %s [ option ... ] dimension [ file ... ]\n", os.Args[0])
@@ -73,7 +75,7 @@ func main() {
 	}
 	if len(flag.Args()) >= 2 {
 		for _, v := range flag.Args()[1:] {
-			r, err := os.Open(v)
+			r, err := os.OpenFile(v, os.O_RDWR, 0)
 			if err != nil {
 				fmt.Fprintf(os.Stderr, "simg: %v\n", err)
 				os.Exit(1)
@@ -83,14 +85,26 @@ func main() {
 				fmt.Fprintf(os.Stderr, "simg: %v\n", err)
 				os.Exit(1)
 			}
-			s := newsimage(img, f, w, h, imaging.Lanczos)
-			err = simg(os.Stdout, s)
+			var s = newsimage(img, f, w, h, imaging.Lanczos)
+			var dest io.Writer
+			if *write {
+				dest = r
+			} else {
+				dest = os.Stdout
+			}
+			err = simg(dest, s)
 			if err != nil {
 				fmt.Fprintf(os.Stderr, "simg: %v\n", err)
 				os.Exit(1)
 			}
+			r.Close()
 		}
 	} else {
+		if *write {
+			// filename is "<stdin>"
+			fmt.Fprintf(os.Stderr, "simg: can't use -w on stdin\n")
+			os.Exit(1)
+		}
 		// TODO(wgr): implement a reader that understands multiple images comming
 		// in from a data stream.
 		img, f, err := image.Decode(os.Stdin)
